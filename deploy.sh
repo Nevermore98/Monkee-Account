006780d2192f55ccce2fc335a8ec4c371a71f04f
Nevermore98 <Nevermorey98@qq.com>
2021-12-17 23:54:24 +0800

    chore: 重命名服务器部署文件

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ git init &&
 git add . &&
 git commit -m "更新部署" &&
 git branch -M main &&
+git remote rm origin &&
 git remote add origin git@github.com:Nevermore98/Monkee-Account-Website.git &&
-git push -f -u origin main &&
+git push -u origin main -f &&
 cd -
 echo https://nevermore98.github.io/Monkee-Account-Website/
