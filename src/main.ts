26f6f63325bb2f9acef252ed35ac95f7508443bd
Nevermorey98 <Nevermorey98@qq.com>
2022-04-13 01:59:06 +0800

    chore: 修改二维码路径

--- src/main.ts
+++ src/main.ts
@@ -60,7 +60,8 @@ app.mount('#app')
 // 非移动端打开页面弹出二维码
 if (document.documentElement.clientWidth > 500) {
   const QRCode = document.createElement('img')
-  QRCode.src = 'src/assets/QR_code.png'
+  QRCode.src =
+    'https://nevermore-picbed-1304219157.cos.ap-guangzhou.myqcloud.com/QR_code.png'
   QRCode.style.position = 'fixed'
   QRCode.style.left = '50%'
   QRCode.style.top = '50%'
